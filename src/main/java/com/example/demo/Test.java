2d03dbf344b7ca8a1f2a8b9304f61a8f00fc679c
yangzhaoguang <994887644@qq.com>
2022-04-03 21:39:30 +0800

    first commit

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+/**
+ * Handsome Man.
+ * <p>
+ * Author: YZG
+ * Date: 2022/4/3 21:28
+ * Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
